5b2585b4aab7e59785ca3e23dff1e57ab2f1c88c
kyleswillard <kyleswillard@gmail.com>
2021-01-06 10:12:44 -0800

    Plant form
    Plant Form with no validation, and no submission.

---
+++ src/components/plant_page/plant_form.component.jsx
@@ -0,0 +1,43 @@
+import React, { useState } from 'react'
+
+const PlantForm = () => {
+    const [plantSearch, setPlantSearch] = useState({
+        name: '',
+        species: '',
+        water_frequency: '',
+    })
+    const handleChanges = (e) => {
+        setPlantSearch({ ...plantSearch, [e.target.name]: e.target.value })
+        console.log(plantSearch)
+    }
+    return (
+        <>
+            <form>
+                <header className="form-header">
+                    <h4>Search for Plants</h4>
+                </header>
+                <label htmlFor="name" />
+                <input
+                    id="name"
+                    type="text"
+                    placeholder="Custom Plant Name"
+                    onChange={handleChanges}
+                />
+                <label htmlFor="species" />
+                <input
+                    id="species"
+                    type="text"
+                    placeholder="Plant Species"
+                    onChange={handleChanges}
+                />
+                <label htmlFor="water_frequency" onChange={handleChanges} />
+                <input
+                    id="water_frequency"
+                    type="text"
+                    placeholder="Watering Frequency"
+                    onChange={handleChanges}
+                />
+            </form>
+        </>
+    )
+}
